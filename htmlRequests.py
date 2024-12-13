4952c17e3e5103393b4800b8330b8f3e8fe2f79f
pekoraptor <filip.ryniewicz@gmail.com>
2024-12-13 23:07:24 +0100

    Working endpoints parsing, template html generations

--- htmlRequests.py
+++ htmlRequests.py
@@ -1,14 +1,26 @@
 from dataclasses import dataclass
+from vulnerability_testers.validate_cmdinjection_vulnerabilities import (
+    validate_cmdinjection_vulnerabilities,
+)
 
 
 @dataclass
 class HtmlRequests:
-    def __init__(self, get=[], post=[], put=[], delete=[], patch=[]):
-        self.get = []
+    def __init__(
+        self, url, stopOnFailure, get=[], post=[], put=[], delete=[], patch=[]
+    ):
+        self.url = url
+        self.stopOnFailure = stopOnFailure
+        self.get = [validate_cmdinjection_vulnerabilities]
         self.post = []
         self.put = []
         self.delete = []
         self.patch = []
 
-def run(self, request):
-    return [vulnerabilityTest(request) for vulnerabilityTest in getattr(self, request.type)]
+    def run(self, request):
+        outputs = []
+        for vulnerabilityTest in getattr(self, request["method"].lower()):
+            test_output = vulnerabilityTest(request)
+            outputs.append(test_output)
+            if self.stopOnFailure and not test_output["passed"]:
+                return outputs
--- html_report/generate_html.py
+++ html_report/generate_html.py
@@ -1,22 +1,56 @@
 import json
 from jinja2 import Template
 import sys
+from pprint import pprint
+
 
 def load_json(json_path):
-    with open(json_path, 'r') as fh:
+    with open(json_path, "r") as fh:
         return json.load(fh)
 
+
+def group_tests(tests):
+    grouped_data = {}
+    for test in tests:
+        test_type = test["testType"]
+        if test_type not in grouped_data:
+            grouped_data[test_type] = {
+                "testsPassed": 0,
+                "testsTotal": 0,
+                "endpoint": [],
+                "description": [],
+            }
+        grouped_data[test_type]["testsTotal"] += 1
+        if test["passed"]:
+            grouped_data[test_type]["testsPassed"] += 1
+            grouped_data[test_type]["endpoint"].append(test["endpoint"])
+            grouped_data[test_type]["description"].append(test["description"])
+    pprint(grouped_data)
+
+    return [
+        (
+            key,
+            value["testsPassed"],
+            value["testsTotal"],
+            value["endpoint"],
+            value["description"],
+        )
+        for key, value in grouped_data.items()
+    ]
+
+
 def generate_html(data, template_path, output_path):
-    print("data", data)
-    with open(template_path, 'r') as fh:
+    print(data)
+    with open(template_path, "r") as fh:
         template = Template(fh.read())
-    
+
     html_content = template.render(data=data)
 
-    with open(output_path, 'w') as fh:
+    with open(output_path, "w") as fh:
         fh.write(html_content)
 
+
 if __name__ == "__main__":
-    # print("ccc")
-    json_data = load_json("../exampleReport.json")
-    generate_html(json_data, "template.html", "generatedReport.html")
+    json_data = load_json("exampleReport.json")
+    grouped_data = group_tests(json_data)
+    generate_html(grouped_data, "template.html", "generatedReport.html")
